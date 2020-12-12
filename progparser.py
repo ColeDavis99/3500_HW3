b38a2ce6f9a0dcad394cc6be092f83ae89a49e8c
cedtfh <cedtfh@mst.edu>
2020-12-12 17:36:18 -0600

    Working on first stopping point

--- progparser.py
+++ progparser.py
@@ -1,3 +1,47 @@
-dog = input()
+import re
+from sys import stdin
 
-print(dog)
+
+
+def retRegex(inp):
+    keywordPat = '(!|:|:=|+|-|*|/|OR|AND|~|(|)|<|>|=|#|;|FUNC|IF|ELSE|PRINT|RETURN|END)'
+    '''
+    identPat =  ''
+    intPat = ''
+    decPat = ''
+    strPat = ''
+    '''
+
+    if(re.match(keywordPat, inp)):
+        print("ITS A KEYWORD!")
+    else:
+        print("NOT A KEYWORD")
+
+
+
+
+
+#Read in input for this program
+inputData = ""
+for line in stdin:
+    inputData += line
+    
+word=""
+for char in inputData:
+    if(char == "\n" or char == " "):
+        print(word, end="")       
+        #Do processing here and stuff
+        
+        #Clear word for next one to identify        
+        word = ""
+
+    else:
+        word += char
+
+'''
+    (WHILE|ELSE|IF|PRINT)               {   pretty_print(tokens);   fout << "KEYWORD    LEXEME: " << yytext << endl;    tokens++;   }
+    [+|-]?[0-9]+                        {   pretty_print(tokens);   fout << "INTCONST   LEXEME: " << yytext << endl;    tokens++;   }
+    [+|-]?[0-9]+\.[0-9]+                {   pretty_print(tokens);   fout << "DECCONST   LEXEME: " << yytext << endl;    tokens++;   }
+    \"[^ \n\t\r]*\"                     {   pretty_print(tokens);   fout << "STRCONST   LEXEME: " << yytext << endl;    tokens++;   }
+    [A-Za-z][^ \n\t\r\"]*               {   pretty_print(tokens);   fout << "IDENT      LEXEME: " << yytext << endl;    tokens++;   }
+'''
