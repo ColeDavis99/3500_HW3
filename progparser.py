dd4c9b365221382fba07f1172668e13d55526501
cedtfh <cedtfh@mst.edu>
2020-12-13 16:16:04 -0600

    Testing some beginning parsing

--- progparser.py
+++ progparser.py
@@ -4,13 +4,26 @@ from sys import stdin
 
 
 #Static Variable in Python (for when getToken is called)
-def staticCtr():
-    staticCtr.counter += 1
+def staticCtr(inc_dec):
+    staticCtr.counter = staticCtr.counter + inc_dec
     return(staticCtr.counter)
 
 #Attribute must be initialized
-staticCtr.counter = -1
+staticCtr.counter = 0
 
+
+#Get the next token to parse
+def getToken(tokenList, inc_dec):
+    print("This is tokenList: " + str(tokenList))
+    print("StaticCtr(0): " + str(staticCtr(0)) + "\nlen(tokenList): " + str(len(tokenList)))
+    print()
+    print()
+
+    if(staticCtr(0) > len(tokenList)):
+        print("Error: Unexpected end of line")
+        return("Error: Unexpected end of line")
+    else:
+        return(tokenList[staticCtr(inc_dec)])
 
 
 def retRegex(word):
@@ -27,60 +40,115 @@ def retRegex(word):
 
     #Whitespace Check
     if(word in spaceList):
-        print("Whitespace, ignore me")
+        #print("Whitespace, ignore me")
         return("Whitespace")
 
     #Keyword check
     if(word in keywordList):
-        print("key")            
+        #print("key")            
         return("key")    
 
     #Int check
     if(re.match(intPat, word)):
-        print("int")
+        #print("int")
         return("int")
 
     #Decimal check
     if(re.match(decPat, word)):
-        print("dec")            
+        #print("dec")            
         return("dec")
 
     #String check
     if(re.match(strPat, word)):
-        print("str")
+        #print("str")
         return("str")
 
     #Identifier
     elif(re.match(identPat, word)):
-        print("ident")        
+        #print("ident")        
         return("ident")
 
     #Random Junk
     else:
-        print("False")
+        #print("False")
         return False
 
 
+
+
+
+#Parse FunctionSequence (START TOKEN)
+def parseFunctionSequence(tokenList):
+    print("In parseFunctionSequence()")
+    parseFunctionDeclaration(tokenList)
+
+#Parse FunctionDeclaration
+def parseFunctionDeclaration(tokenList):
+    print("In parseFunctionDeclaration()")
+    token = getToken(tokenList, 1)
+    
+    if(token == "FUNC"):
+        token = getToken(tokenList, 1)
+        if(retRegex(token) == "ident"):
+            token = getToken(tokenList, 1)
+            if(token == "("):
+                parseParamSequence(tokenList)
+                token = getToken(tokenList, 1)
+                if(token == ")"):
+                    parseStatementSequence(tokenList)
+                else:
+                    return("Error parsing function declaration, expecting \")\"")
+            else:
+                return("Error parsing function declaration, expecting \"(\"")
+        else:
+            return("Error parsing function declaration, expecting identifier")        
+    else:
+        return("Error parsing function declaration, expecting FUNC")
+
+#Parse ParamSequence
+def parseParamSequence(tokenList):
+    print("In parseParamSequence()")
+    beenLooping = False
+    token = getToken(tokenList, 1)
+
+    if(retRegex(token) == "ident"):
+        token = getToken(tokenList, 1)
+        while(token == ","):
+            beenLooping = True
+            token = getToken(tokenList, 1)
+            if(retRegex(token) == "ident"):
+                pass
+            else:
+                return("Error parsing param sequence, expecting identifier (debug:2)")
+
+            token = getToken(tokenList, 1)
+
+    else:
+        return("Error parsing param sequence, expecting identifier (debug:1)")
+
+    if(beenLooping):
+        getToken(tokenList, -1) #Back up the static var one so we dont mess up tokenList counter
+    
+
 #Parse Relation
-def parseRelation(tokenList, token):
+def parseRelation(tokenList):
+    print("In parseRelation()")
+    token = getToken(tokenList, 1)    
     relationList = ["<", ">", "=", "#"]    
+    
     if(token in relationList):
         pass
     else:
         print("Error parsing relation: Expected <, >, =, or #")
+    
+#Parse StatementSequence
+def parseStatementSequence(tokenList):
+    print("In parseStatementSequence()")
+    parseStatement(tokenList)
 
-#Parse FunctionSequence (START TOKEN)
-def parseFunctionSequence(tokenList, token):
-    print("This is tokenList: ")
-    print(tokenList)
-    print("This is token: " + token)
-
-#Parse FunctionDeclaration
-def parseFunctionDeclaration(tokenList, token):
-    if(token == "FUNC"):
-        pass        
-
-
+def parseStatement(tokenList):
+    print("In parseStatement()")
+    pass    
 
 ####################################
 #Read in input for this program
@@ -107,13 +175,16 @@ for char in inputData:
 
 #Remove the whitespace from tokenlist
 for token in tokenList:
-    tokenList.remove("")
-    tokenList.remove('')
+    if(token == ""):
+        tokenList.remove("")
+    if(token == ''):
+        tokenList.remove('')
 
 
 
 ##########################
 #Start the actual parsing
 ##########################
-parseFunctionSequence(tokenList, tokenList[staticCtr()])
+parseFunctionSequence(tokenList)
+
 
