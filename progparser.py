9be7c773f769e1aac8f4268a332848a129aa45a5
cedtfh <cedtfh@mst.edu>
2020-12-12 19:13:20 -0600

    Regex working properly. Grammar time

--- progparser.py
+++ progparser.py
@@ -4,7 +4,7 @@ from sys import stdin
 
 
 def retRegex(word):
-    keywordList = ["!", ":" ,":=", "+", "-", "*", "/", "OR", "AND", "~", "(", ")", "<", ">", "=", "#", ";", "FUNC", "IF", "ELSE", "WHILE", "PRINT", "RETURN", "END"]    
+    keywordList = ["!", ":", ":=", "+", "-", "*", "/", "OR", "AND", "~", "(", ")", "<", ">", "=", "#", ";", "FUNC", "IF", "ELSE", "WHILE", "PRINT", "RETURN", "END"]    
     intPat = '^[+|-]?[0-9]+$'
     decPat = '^[+|-]?[0-9]+[.][0-9]+$'
     strPat = '^[\"][\S]*[\"]$'
@@ -13,38 +13,28 @@ def retRegex(word):
 
     #Keyword check
     if(word in keywordList):              
-        print("Its a KEYWORD!") 
-    else:
-        print("Not a KEYWORD")
-    
+        return("key")    
 
     #Int check
     if(re.match(intPat, word)):
-        print("Its an INT")
-    else:
-        print("Not an int")
+        return("int")
 
     #Decimal check
     if(re.match(decPat, word)):
-        print("Its a DEC")
-    else:
-        print("Not a DEC")
+        return("dec")
 
     #String check
     if(re.match(strPat, word)):
-        print("Its a STRING")
-    else:
-        print("Not a STRING")
-
+        return("str")
 
     #Identifier
-    if(re.match(identPat, word)):
-        print("Its an identifier")
-    else:
-        print("Not an identifier")
+    elif(re.match(identPat, word)):
+        return("ident")
 
-    print(word)
-    print()
+    else:
+        return False
+
+
 
 ####################################
 #Read in input for this program
@@ -60,9 +50,10 @@ for line in stdin:
 word=""
 for char in inputData:
     if(char == "\n" or char == " "):
-        #print(word, end="")       
+        #print(word, end=": ")       
+        
         #Do processing here and stuff
-        retRegex(word)
+        #retRegex(word)
         
         #Clear word for next one to identify        
         word = ""
@@ -70,10 +61,3 @@ for char in inputData:
     else:
         #Continue building the word, char by char
         word += char
-
-'''
-    [+|-]?[0-9]+                        {   pretty_print(tokens);   fout << "INTCONST   LEXEME: " << yytext << endl;    tokens++;   }
-    [+|-]?[0-9]+\.[0-9]+                {   pretty_print(tokens);   fout << "DECCONST   LEXEME: " << yytext << endl;    tokens++;   }
-    \"[^ \n\t\r]*\"                     {   pretty_print(tokens);   fout << "STRCONST   LEXEME: " << yytext << endl;    tokens++;   }
-    [A-Za-z][^ \n\t\r\"]*               {   pretty_print(tokens);   fout << "IDENT      LEXEME: " << yytext << endl;    tokens++;   }
-'''
