5898ef157fb43352ec91bb6f4c67332092d58eac
cedtfh <cedtfh@mst.edu>
2020-12-13 10:04:42 -0600

    Whitespace should be ignored now0

--- progparser.py
+++ progparser.py
@@ -9,29 +9,45 @@ def retRegex(word):
     decPat = '^[+|-]?[0-9]+[.][0-9]+$'
     strPat = '^[\"][\S]*[\"]$'
     identPat = '^[a-zA-z][a-zA-z0-9]*$'
+    spaceList = [""] #Whitespace is ignored when concatenating "word" earlier in the program. It gets thrown to this function as an empty string.
 
+
+    #print("Word:",end="")
+    #print(word + ":")
+
+    #Whitespace Check
+    if(word in spaceList):
+        print("Whitespace, ignore me")
+        return("Whitespace")
 
     #Keyword check
-    if(word in keywordList):              
+    if(word in keywordList):
+        print("key")            
         return("key")    
 
     #Int check
     if(re.match(intPat, word)):
+        print("int")
         return("int")
 
     #Decimal check
     if(re.match(decPat, word)):
+        print("dec")            
         return("dec")
 
     #String check
     if(re.match(strPat, word)):
+        print("str")
         return("str")
 
     #Identifier
     elif(re.match(identPat, word)):
+        print("ident")        
         return("ident")
 
+    #Random Junk
     else:
+        print("False")
         return False
 
 
@@ -49,11 +65,11 @@ for line in stdin:
 ######################################################
 word=""
 for char in inputData:
-    if(char == "\n" or char == " "):
+    if(char == "\n" or char == " " or char == "\t"):
         #print(word, end=": ")       
         
         #Do processing here and stuff
-        #retRegex(word)
+        retRegex(word)
         
         #Clear word for next one to identify        
         word = ""
